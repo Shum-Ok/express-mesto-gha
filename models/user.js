9948d90ed85693b8b49bd66eea53919fda7984c2
ShumOk <shum_ok@bk.ru>
2022-06-16 14:33:27 +0300

    проверка валидация 0.2

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: [isURL, 'Ошибка валидации']б
+    validate: [isURL, 'Ошибка валидации'],
   },
   email: {
     type: String,
